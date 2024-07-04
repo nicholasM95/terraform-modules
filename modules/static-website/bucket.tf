3425b2bab5d06e3ea2535f5d7ae05851784be8c2
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-07-05 00:48:33 +0200

    chore: add manifest

--- modules/static-website/bucket.tf
+++ modules/static-website/bucket.tf
@@ -1,14 +1,15 @@
 locals {
   mime_types = {
-    "css"  = "text/css"
-    "html" = "text/html"
-    "ico"  = "image/vnd.microsoft.icon"
-    "js"   = "application/javascript"
-    "json" = "application/json"
-    "map"  = "application/json"
-    "png"  = "image/png"
-    "svg"  = "image/svg+xml"
-    "txt"  = "text/plain"
+    "css"         = "text/css"
+    "html"        = "text/html"
+    "ico"         = "image/vnd.microsoft.icon"
+    "js"          = "application/javascript"
+    "json"        = "application/json"
+    "map"         = "application/json"
+    "png"         = "image/png"
+    "svg"         = "image/svg+xml"
+    "txt"         = "text/plain"
+    "webmanifest" = "application/json"
   }
 }
 
