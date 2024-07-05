820db666bcfd8d7e8f920b7e395dab6247a697b9
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-07-05 23:26:09 +0200

    chore: update postgres engine version

--- modules/cloud-database/database.tf
+++ modules/cloud-database/database.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "database" {
   identifier                          = var.database_name
   db_name                             = var.database_name
   engine                              = "postgres"
-  engine_version                      = "14.8"
+  engine_version                      = "14.9"
   instance_class                      = "db.t3.micro"
   username                            = "master"
   password                            = random_password.password.result
