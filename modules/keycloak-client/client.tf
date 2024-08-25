9a4dbb9512ce7a39a7dcfeee13bd42ffb30110ea
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-08-25 23:38:23 +0200

    feat: add keycloak client

---
+++ modules/keycloak-client/client.tf
@@ -0,0 +1,13 @@
+resource "keycloak_openid_client" "openid_client" {
+  realm_id  = var.realm_id
+  client_id = var.client_id
+
+  name    = var.client_name
+  enabled = true
+
+  access_type         = "PUBLIC"
+  valid_redirect_uris = var.valid_redirect_uris
+
+  login_theme           = "keycloak"
+  standard_flow_enabled = true
+}
