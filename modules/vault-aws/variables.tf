e8387e8e3ca293e6f0098cb641f1ab8b929ed099
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-09-07 02:00:25 +0200

    chore: add aws auth backend client

--- modules/vault-aws/variables.tf
+++ modules/vault-aws/variables.tf
@@ -7,3 +7,13 @@ variable "vault_path" {
   description = "Vault path"
   type        = string
 }
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
--- modules/vault-aws/vault.tf
+++ modules/vault-aws/vault.tf
@@ -9,3 +9,11 @@ resource "vault_aws_auth_backend_role" "auth_aws_backend_role" {
   bound_iam_principal_arns = [var.role_id]
   token_policies           = [var.vault_path]
 }
+
+resource "vault_aws_auth_backend_client" "auth_aws_backend_client" {
+  backend                    = vault_auth_backend.auth_aws_backend.path
+  access_key                 = var.access_key
+  secret_key                 = var.secret_key
+  sts_region                 = "eu-west-1"
+  use_sts_region_from_client = true
+}
