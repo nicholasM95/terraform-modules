2156291f2c7b6be224444177091c84f2f59f55c3
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-10-09 22:54:43 +0200

    chore: rename rewrite function

--- modules/static-website/rewrite.tf
+++ modules/static-website/rewrite.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_function" "rewrite_function" {
   count   = var.enable_rewrite ? 1 : 0
-  name    = "rewrite-url-${var.project_name}"
+  name    = "${var.project_name}-rewrite-url"
   runtime = "cloudfront-js-2.0"
   publish = true
   code    = file("${path.module}/function.js")
