4d572c7c79c9533a23f581068751fab8f3763c6e
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-10-09 23:20:22 +0200

    chore: fix auth

--- modules/static-website/auth.tf
+++ modules/static-website/auth.tf
@@ -2,8 +2,5 @@ resource "aws_cloudfront_function" "basic_auth_function" {
   count   = var.enable_basic_auth ? 1 : 0
   name    = "${var.project_name}-basic-auth-function"
   runtime = "cloudfront-js-1.0"
-
-  code = <<EOF
-
-EOF
+  code    = file("${path.module}/auth.js")
 }
