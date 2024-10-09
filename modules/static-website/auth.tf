2b4f66add57a9805134d30e1d25d28ea6d468d97
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-10-09 23:39:56 +0200

    chore: refactor auth

--- modules/static-website/auth.tf
+++ modules/static-website/auth.tf
@@ -2,5 +2,35 @@ resource "aws_cloudfront_function" "basic_auth_function" {
   count   = var.enable_basic_auth ? 1 : 0
   name    = "${var.project_name}-basic-auth-function"
   runtime = "cloudfront-js-1.0"
-  code    = file("${path.module}/auth.js")
+
+  code = <<EOF
+function handler(event) {
+    var request = event.request;
+    var headers = request.headers;
+    var uri = request.uri;
+
+    var authHeader = headers['authorization'];
+    var encodedCredentials = '${var.basic_auth_username_password}';
+
+    if (authHeader && authHeader.value === 'Basic ' + encodedCredentials) {
+        if (uri.endsWith('/')) {
+            request.uri += 'index.html';
+        }
+        else if (!uri.includes('.')) {
+            request.uri += '/index.html';
+        }
+        return request;
+    }
+
+    return {
+        statusCode: 401,
+        statusDescription: 'Unauthorized',
+        headers: {
+            'www-authenticate': {
+                value: 'Basic realm="Secure Area"'
+            }
+        }
+    };
+}
+EOF
 }
