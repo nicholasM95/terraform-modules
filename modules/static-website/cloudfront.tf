15a5a7055c16b01ed6e29446c9b5106a126fc5f5
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-03-04 10:05:31 +0100

    chore: remove duplicated ssl cert

--- modules/static-website/cloudfront.tf
+++ modules/static-website/cloudfront.tf
@@ -64,9 +64,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-    acm_certificate_arn            = aws_acm_certificate.cert.arn
-    ssl_support_method             = "sni-only"
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
   }
 
   web_acl_id = var.web_acl_id
