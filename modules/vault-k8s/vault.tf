9c649a0507388d3bd53edf981f93af67b5a5769a
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-06-19 00:52:40 +0200

    chore: update vault backend k8s

--- modules/vault-k8s/vault.tf
+++ modules/vault-k8s/vault.tf
@@ -1,10 +1,9 @@
-resource "vault_auth_backend" "auth_k8s_backend" {
-  type = "kubernetes"
-  path = var.vault_path
+data "vault_auth_backend" "kubernetes" {
+  path = "kubernetes"
 }
 
 resource "vault_kubernetes_auth_backend_role" "auth_kubernetes_backend_role" {
-  backend                          = vault_auth_backend.auth_k8s_backend.path
+  backend                          = data.vault_auth_backend.kubernetes.path
   role_name                        = var.vault_path
   bound_service_account_names      = [var.service_account_name]
   bound_service_account_namespaces = [var.service_account_namespace]
