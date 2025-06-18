33212505751e29fc63452bdbe4f2d985a3f2b896
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-06-18 23:27:53 +0200

    chore: remove auth_kubernetes_backend_config in vault-k8s

--- modules/vault-k8s/variables.tf
+++ modules/vault-k8s/variables.tf
@@ -3,16 +3,6 @@ variable "vault_path" {
   type        = string
 }
 
-variable "kubernetes_ca_cert" {
-  description = "Kubernetes ca cert"
-  type        = string
-}
-
-variable "kubernetes_internal_host" {
-  description = "Kubernetes internal host"
-  type        = string
-}
-
 variable "service_account_name" {
   description = "Service account name"
   type        = string
