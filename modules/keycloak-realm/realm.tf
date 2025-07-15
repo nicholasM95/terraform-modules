7656312fb41a7be600f5d88b232fd611af0d5039
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-15 21:50:47 +0200

    feat: set default auth flow to passwordless

--- modules/keycloak-realm/realm.tf
+++ modules/keycloak-realm/realm.tf
@@ -2,4 +2,19 @@ resource "keycloak_realm" "realm" {
   realm        = var.realm_name
   enabled      = true
   display_name = var.realm_name
+
+  browser_flow = keycloak_authentication_flow.passwordless_browser.alias
+}
+
+resource "keycloak_authentication_flow" "passwordless_browser" {
+  realm_id    = keycloak_realm.realm.id
+  alias       = "passwordless-browser"
+  description = "Browser flow for passwordless authentication"
+}
+
+resource "keycloak_authentication_execution" "webauthn_passwordless" {
+  realm_id          = keycloak_realm.realm.id
+  parent_flow_alias = keycloak_authentication_flow.passwordless_browser.alias
+  authenticator     = "webauthn-passwordless-authenticator"
+  requirement       = "REQUIRED"
 }
