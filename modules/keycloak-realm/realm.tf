935aab6480d823a104b9646d13bb7761fa3e2f37
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-15 22:02:47 +0200

    chore: refactor passwordless realm

--- modules/keycloak-realm/realm.tf
+++ modules/keycloak-realm/realm.tf
@@ -4,10 +4,19 @@ resource "keycloak_realm" "realm" {
   display_name = var.realm_name
 }
 
+resource "keycloak_required_action" "webauthn_register_passwordless" {
+  realm_id = keycloak_realm.realm.id
+  alias    = "webauthn-register-passwordless"
+  name     = "Webauthn Register Passwordless"
+  enabled  = true
+}
+
 resource "keycloak_authentication_flow" "passwordless_browser" {
   realm_id    = keycloak_realm.realm.id
   alias       = "passwordless-browser"
   description = "Browser flow for passwordless authentication"
+
+  depends_on = [keycloak_required_action.webauthn_register_passwordless]
 }
 
 resource "keycloak_authentication_execution" "webauthn_passwordless" {
@@ -15,4 +24,6 @@ resource "keycloak_authentication_execution" "webauthn_passwordless" {
   parent_flow_alias = keycloak_authentication_flow.passwordless_browser.alias
   authenticator     = "webauthn-passwordless-authenticator"
   requirement       = "REQUIRED"
+
+  depends_on = [keycloak_required_action.webauthn_register_passwordless]
 }
