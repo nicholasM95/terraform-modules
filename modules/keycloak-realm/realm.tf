9cc5d51613354b618f3da70eeccd3e5a842b3684
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-15 22:23:51 +0200

    chore: enable webauth

--- modules/keycloak-realm/realm.tf
+++ modules/keycloak-realm/realm.tf
@@ -4,26 +4,15 @@ resource "keycloak_realm" "realm" {
   display_name = var.realm_name
 }
 
-resource "keycloak_required_action" "webauthn_register_passwordless" {
-  realm_id = keycloak_realm.realm.id
-  alias    = "webauthn-register-passwordless"
-  name     = "Webauthn Register Passwordless"
-  enabled  = true
-}
-
 resource "keycloak_authentication_flow" "passwordless_browser" {
   realm_id    = keycloak_realm.realm.id
   alias       = "passwordless-browser"
   description = "Browser flow for passwordless authentication"
-
-  depends_on = [keycloak_required_action.webauthn_register_passwordless]
 }
 
 resource "keycloak_authentication_execution" "webauthn_passwordless" {
   realm_id          = keycloak_realm.realm.id
   parent_flow_alias = keycloak_authentication_flow.passwordless_browser.alias
-  authenticator     = "webauthn-passwordless-authenticator"
+  authenticator     = "webauthn-authenticator-passwordless"
   requirement       = "REQUIRED"
-
-  depends_on = [keycloak_required_action.webauthn_register_passwordless]
 }
