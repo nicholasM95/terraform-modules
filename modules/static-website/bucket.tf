c05a64b720faa022aed6e722a6f9ce0e5d0fcb9c
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-08-07 12:45:17 +0200

    chore: add mime types for static hosting

--- modules/static-website/bucket.tf
+++ modules/static-website/bucket.tf
@@ -7,6 +7,8 @@ locals {
     "json"        = "application/json"
     "map"         = "application/json"
     "png"         = "image/png"
+    "jpg"         = "image/jpeg"
+    "jpeg"         = "image/jpeg"
     "svg"         = "image/svg+xml"
     "txt"         = "text/plain"
     "webmanifest" = "application/json"
