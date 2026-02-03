d6262a822dacfc68b3bffaca2dcc2415bd1ac070
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-02-03 18:08:35 +0100

    feat: add keycloak client resource server

---
+++ modules/keycloak-client-resource-server/client.tf
@@ -0,0 +1,9 @@
+resource "keycloak_openid_client" "openid_client" {
+  realm_id  = var.realm_id
+  client_id = var.client_id
+
+  name    = var.client_name
+  enabled = true
+
+  access_type         = "CONFIDENTIAL"
+}
---
+++ modules/keycloak-client-resource-server/variables.tf
@@ -0,0 +1,14 @@
+variable "realm_id" {
+  type        = string
+  description = "id of the keycloak realm"
+}
+
+variable "client_id" {
+  type        = string
+  description = "client id"
+}
+
+variable "client_name" {
+  type        = string
+  description = "client name"
+}
