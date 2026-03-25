04dab3b7aecb74dbe256cadd4e5ffd05398cbea2
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-03-25 22:16:27 +0100

    chore: add openid as client scope in keycloak

--- modules/keycloak-realm-passwordless/realm.tf
+++ modules/keycloak-realm-passwordless/realm.tf
@@ -20,3 +20,22 @@ resource "keycloak_authentication_execution" "webauthn_passwordless" {
   authenticator     = "webauthn-authenticator-passwordless"
   requirement       = "REQUIRED"
 }
+
+resource "keycloak_openid_client_scope" "openid" {
+  realm_id    = keycloak_realm.realm.id
+  name        = "openid"
+  description = "OpenID"
+}
+
+resource "keycloak_realm_optional_client_scopes" "optional_scopes" {
+  realm_id = keycloak_realm.realm.id
+
+  optional_scopes = [
+    "address",
+    "phone",
+    "offline_access",
+    "microprofile-jwt",
+    "organization",
+    keycloak_openid_client_scope.openid.name
+  ]
+}
